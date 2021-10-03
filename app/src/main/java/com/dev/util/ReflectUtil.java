a5c376dcf64395414c43ddf1fbc6af471e2fc7ee
David <18588744489@163.com>
2021-10-03 18:15:04 +0800

    Update

--- app/src/main/java/com/dev/util/ReflectUtil.java
+++ app/src/main/java/com/dev/util/ReflectUtil.java
@@ -1,5 +1,6 @@
 package com.dev.util;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class ReflectUtil {
@@ -7,9 +8,9 @@ public class ReflectUtil {
         try {
             Class<?> c = Class.forName(className);
             if (params != null) {
-                int plength = params.length;
-                Class[] paramsTypes = new Class[plength];
-                for (int i = 0; i < plength; i++) {
+                int length = params.length;
+                Class[] paramsTypes = new Class[length];
+                for (int i = 0; i < length; i++) {
                     paramsTypes[i] = params[i].getClass();
                 }
                 Method method = c.getDeclaredMethod(methodName, paramsTypes);
@@ -19,6 +20,28 @@ public class ReflectUtil {
             Method method = c.getDeclaredMethod(methodName);
             method.setAccessible(true);
             return method.invoke(instance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object getInstance(String className, Object... params) {
+        try {
+            Class<?> c = Class.forName(className);
+            if (params != null) {
+                int length = params.length;
+                Class[] paramsTypes = new Class[length];
+                for (int i = 0; i < length; i++) {
+                    paramsTypes[i] = params[i].getClass();
+                }
+                Constructor constructor = c.getDeclaredConstructor(paramsTypes);
+                constructor.setAccessible(true);
+                return constructor.newInstance(params);
+            }
+            Constructor constructor = c.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
         } catch (Exception e) {
             e.printStackTrace();
         }
